0661d041db3b40d72d649323f36f467d7219eaad
Raul Ramirez <admin@local.host>
2014-03-08 23:47:24 -0500

    added PDO and MySQLi test cases working

---
+++ PDO_test.php
@@ -0,0 +1,34 @@
+<?php
+
+echo 'echoing PDO <br/>';
+
+
+//  from local php -  local Mysql - working
+
+// $db = new PDO(
+//   'mysql:host=localhost:8889;dbname=gae_test;charset=utf8',
+//   'root',
+//   'root'
+// );
+
+//  local php - Cloud SQL - working
+
+// $db = new PDO(
+//   'mysql:host=173.194.83.192;dbname=gae_test;charset=utf8',
+//   'root',
+//   'root'
+// );
+
+
+// From GAE to Cloud SQl -  working
+$db = new PDO(
+  'mysql:unix_socket=/cloudsql/round-device-509:cloudsqltest;dbname=gae_test;charset=utf8',
+  'root',
+  ''
+);
+
+
+
+ foreach($db->query('SELECT * FROM gae_table') as $row){
+      echo $row['id'].' '.$row['name']."<br/>";
+} 
---
+++ mysqli_test.php
@@ -0,0 +1,26 @@
+<?php
+
+echo 'echoing mysqli </br>';
+
+// local php - local mysql - working
+// $mysqli = new mysqli('localhost:8889', 'root', 'root', 'gae_test');
+  
+
+// local php - Cloud SQL - working
+
+// $mysqli = new mysqli('173.194.83.192', 'root', 'root', 'gae_test');
+
+
+// GAE - Cloud SQL
+
+$mysqli = new mysqli(null, 'root', '', 'gae_test', null, '/cloudsql/round-device-509:cloudsqltest');
+
+
+$query = "SELECT * FROM gae_table";
+$result = $mysqli->query($query) or die ($mysqli->error.__LINE__);
+
+if($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo stripslashes($row['id']).' '.stripslashes($row['name']).'<br/>';
+  }
+}
